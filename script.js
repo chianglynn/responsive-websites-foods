a5a27c1ad5e023d85072cea2e5afccefc547a456
Chiang Lynn <lynnchiangtw@gmail.com>
2021-01-21 20:31:48 +0800

    Add footer and navigation sections.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item) => {
+        item.classList.toggle('change');
+    })
+})
+
 const icons = document.querySelectorAll('.section-1-icons i');
 let i = 1;
 
@@ -12,3 +18,4 @@ setInterval(() => {
         icon.nextElementSibling.classList.add('change')
     }
 }, 4000)
+
