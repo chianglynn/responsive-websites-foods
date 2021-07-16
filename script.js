08635e4c9de71dd5f828831dc76eca3ba0ccb1da
Chiang Lynn <lynnchiangtw@gmail.com>
2021-07-16 12:08:09 +0800

    Rearrange code

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-document.querySelector('.menu').addEventListener('click', () => {
-    document.querySelectorAll('.target').forEach((item) => {
-        item.classList.toggle('change');
-    })
-})
-
+const menu = document.querySelector('.menu');
+const targets = document.querySelectorAll('.target');
 const icons = document.querySelectorAll('.section-1-icons i');
-let i = 1;
+let iconIndex = 1;
+
+function toggleNavbar() {
+    targets.forEach(item => item.classList.toggle('change'));
+}
 
 setInterval(() => {
-    i++;
     const icon = document.querySelector('.section-1-icons .change');
-    icon.classList.remove('change')
-    if (i > icons.length) {
-        icons[0].classList.add('change')
-        i = 1;
+    icon.classList.remove('change');
+    iconIndex++;
+    if (iconIndex > icons.length) {
+        icons[0].classList.add('change');
+        iconIndex = 1;
     } else {
-        icon.nextElementSibling.classList.add('change')
+        icon.nextElementSibling.classList.add('change');
     }
-}, 4000)
+}, 4000);
 
+menu.addEventListener('click', toggleNavbar);
